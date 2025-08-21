ae63e6a472dd07e481ba15a3fcdec2d7d31925b2
tushar <jaglekhsolanki@gmail.com>
2025-08-21 12:11:44 +0530

    fix: removed extra web client bean

--- src/main/java/com/example/roledefine/config/WebClientConfig.java
+++ src/main/java/com/example/roledefine/config/WebClientConfig.java
@@ -9,14 +9,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-//    @Bean
-//    public WebClient flightWebClient() {
-//        return WebClient.builder()
-//                .baseUrl("https://travelnext.works/api/")
-//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-//                .build();
-//    }
-
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
